7cc3d152c80d13cc4370505c9355a0950915490d
ankithkumar <ankith.b01@gmail.com>
2019-03-05 00:51:20 +0530

    thumbnail image added

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
     selector: 'app-image',
@@ -9,7 +10,9 @@ import { Component, Input } from '@angular/core';
 export class ImageComponent {
     @Input('image') imageSrc: string;
     @Input('description') description: string;
-
+    defaultImage = '/assets/images/placeholder.jpg';
+    offset = 100;
+    
     constructor() {
         console.log('image component constructor');
         this.imageSrc = 'https://image.shutterstock.com/image-photo/boy-kicks-football-during-game-450w-693609082.jpg';
